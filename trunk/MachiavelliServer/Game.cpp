2b7d06f3aa4667a01014fe1fe0a38ed563df5cd0
ajpserva@avans.nl <ajpserva@avans.nl@bf9a6662-2989-3291-5831-2b3c4df332ee>
2015-01-05 14:21:12 +0000

    First commit in Google Code: + Added necessary classes according to UML diagram + Added template class CardStack + Added one 'simple' method in template class to add any given cards to a CardStack

---
+++ trunk/MachiavelliServer/Game.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "Game.h"
+
+//Testing
+#include "BuildingCard.h"
+
+Game::Game()
+{
+	BuildingCard newbuildingCard;
+
+	buildings.add(newbuildingCard);
+
+}
+
+Game::~Game()
+{
+}
+
+
+void Game::initServer()
+{
+
+}
+
+void Game::waitForClients()
+{
+
+}
+
+void Game::run()
+{
+
+}
+
+void Game::initGame()
+{
+
+}
+
+void Game::dealCharacters()
+{
+
+}
+
+void Game::discardCharacter()
+{
+
+}
+
+void Game::showCharacter()
+{
+
+}
+
+void Game::pickCharacter()
+{
+
+}
+
+void Game::playRound()
+{
+
+}
+
+void Game::countPlayerScores()
+{
+
+}
+
+void Game::playCharacter()
+{
+
+}
